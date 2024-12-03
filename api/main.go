4917c7a25290c6d966cb83d62c427f5e032abc63
jacksonlmp <jacksonlmp17.jl@gmail.com>
2024-12-03 19:23:05 -0300

    Creating router module

--- api/main.go
+++ api/main.go
@@ -1,7 +1,16 @@
 package main
 
-import "fmt"
+import (
+	"api/src/router"
+	"fmt"
+	"log"
+	"net/http"
+)
 
 func main() {
 	fmt.Println("Running API")
+
+	r := router.Gerar()
+
+	log.Fatal(http.ListenAndServe(":8000", r))
 }
